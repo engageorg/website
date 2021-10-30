fabb1e93745c347e2f27648a81ed1dc0cea241fc
Aman <54789531+eppisai@users.noreply.github.com>
2021-10-31 02:59:09 +0530

    Update nav.js

--- website/src/component/nav.js
+++ website/src/component/nav.js
@@ -3,16 +3,16 @@ import React from 'react';
 function nav() {
     return ( 
         <div data-scroll-section>
-    <nav class="navbar">
-        <ul>
-            <img class="company-logo "src="https://www.eppisai.com/static/media/logo_light.4875494a.svg" alt="logo"/>
-             <li>About</li>  
-             <li>Explore</li>
-             <li>Resources</li> 
-             <li>Pricing</li>
-             <button>Jump right in!</button>
-        </ul>
-    </nav>
+            <nav class="navbar">
+                <ul>
+                    <img class="company-logo "src="https://www.eppisai.com/static/media/logo_light.4875494a.svg" alt="logo"/>
+                    <li>About</li>  
+                    <li>Explore</li>
+                    <li>Resources</li> 
+                    <li>Pricing</li>
+                    <button>Jump right in!</button>
+                </ul>
+            </nav>
         </div>
      );
 }
